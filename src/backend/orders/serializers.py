b5c953a34b0c1d97c3bf7b5bcdfccaa669f17374
jyothisbenny <jyothisbenny75@gmail.com>
2024-08-20 21:21:39 +0530

    clear cart added

--- src/backend/orders/serializers.py
+++ src/backend/orders/serializers.py
@@ -38,13 +38,16 @@ class OrderSerializer(ModelSerializer):
 
     def create(self, validated_data):
         total_amount = 0
-
+        product_list = []
+        user = validated_data.get('user')
         items = validated_data.pop('items', [])
         items_values = []
         for record in items:
             record.pop('id', None)
 
             product = record.get('product', None)
+            product_list.append(product)
+
             product_price = product.price or 0
             record['amount'] = product_price
             total_amount += product_price
@@ -54,6 +57,9 @@ class OrderSerializer(ModelSerializer):
         instance = Order.objects.create(**validated_data)
         instance.items.set(items_values)
         instance.save()
+
+        # clear cart
+        Cart.objects.filter(user=user, product__in=product_list, is_active=True).update(is_active=False)
         return instance
 
     @staticmethod
